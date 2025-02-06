1404dfa5588cd5f1a4abde25e8ee5bd14ddec993
Santiago Figueroa <developersantiagofigueroa@gmail.com>
2025-02-06 03:29:12 -0300

    Transiciones en el SubNav

--- src/components/SubNav.jsx
+++ src/components/SubNav.jsx
@@ -3,16 +3,16 @@ const SubNav = () => {
     return (
         <div className="hidden -mt-1 bg-gradient-to-b from-[#2b2b7b] to-[#1b1b51] md:flex justify-center gap-25 p-4 text-white font-medium">
             <div className='flex gap-5'>
-                <a href="#">Destacado</a>
-                <a href="#">Contacto</a>
-                <a href="#">Favoritos</a>
-                <a href="#">Ayuda</a>
+                <a href="#" className='hover:text-[#ff2ed4] transition-all'>Destacado</a>
+                <a href="#" className='hover:text-[#ff2ed4] transition-all'>Contacto</a>
+                <a href="#" className='hover:text-[#ff2ed4] transition-all'>Favoritos</a>
+                <a href="#" className='hover:text-[#ff2ed4] transition-all'>Ayuda</a>
             </div>
 
             <div className='flex gap-5'>
-                <a href="#" className='bg-[#ff2ed4] rounded-[20px] px-3 '>Crea tu cuenta</a>
-                <a href="#">Ingresar</a>
-                <a href="#">Mis compras</a>
+                <a href="#" className='bg-[#ff2ed4] rounded-[20px] px-3 hover:bg-purple-800 transition-all'>Crea tu cuenta</a>
+                <a href="#" className='hover:text-[#ff2ed4] transition-all'>Ingresar</a>
+                <a href="#" className='hover:text-[#ff2ed4] transition-all'>Mis compras</a>
             </div>
             
         </div>
