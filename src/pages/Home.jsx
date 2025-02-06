7cb9ad01441214931ce720ab8c8ebc7c3da9c8ee
Santiago Figueroa <developersantiagofigueroa@gmail.com>
2025-02-06 03:37:31 -0300

    Diferente imagen para desktop para no mezclar el logo nuevamente del navbar con el del home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Logo from '../images/Home.png'
+import LogoDesktop from '../images/HomeDesktop.png'
 const Home = () => {
   return (
     <div>
         <div className='bg-gradient-to-b from-[#1b1b51] to-[#ff2ad9] flex justify-center'>
-            <img src={Logo} className='w-screen py-1 md:w-[650px]' />
+            <img src={Logo} className='w-screen py-1 md:w-[650px] lg:hidden' />
+            <img src={LogoDesktop} className='hidden lg:block w-screen py-1 lg:w-[650px]' />
         </div>
     </div>
   )
